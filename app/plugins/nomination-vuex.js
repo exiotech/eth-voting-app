35db408cec2466c7fd808dd40c4bc55d52134f7e
@19Rom94 <roman.sargsyan@exio.tech>
2019-03-16 12:08:09 +0400

    Election frontt

--- app/plugins/nomination-vuex.js
+++ app/plugins/nomination-vuex.js
@@ -25,7 +25,6 @@ export default function nominationPeriodPlagin(web3) {
                       }
                     });
                   }, 1000)
-                  console.log(event);
                 })
               }
             })
